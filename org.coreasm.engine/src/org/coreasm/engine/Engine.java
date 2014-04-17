f5c4a7d3063fc317e21f18fe57279bc20acec056
marcel.dausend <marcel.dausend@uni-ulm.de>
2014-04-17 14:39:48 +0200

    update of users manual -fix: the version number of engine now matches the maven version

--- org.coreasm.engine/src/org/coreasm/engine/Engine.java
+++ org.coreasm.engine/src/org/coreasm/engine/Engine.java
@@ -85,7 +85,7 @@ import org.coreasm.util.Tools;
  */
 public class Engine implements ControlAPI {
 
-	public static final VersionInfo VERSION_INFO = new VersionInfo(1, 6, 0, "beta");
+	public static final VersionInfo VERSION_INFO = new VersionInfo(1, 6, 2, "beta");
 
 	private static final Logger logger = LoggerFactory.getLogger(Engine.class);
 
