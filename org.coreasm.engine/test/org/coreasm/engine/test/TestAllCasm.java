351c399ee6bd39d5bfbb8f83362175062e235c79
marcel.dausend <marcel.dausend@uni-ulm.de>
2014-07-09 16:44:59 +0200

    introduced junit test -junit tests are used to execute annotated CoreASM specifications in test-rsc
    Signed-off-by: marcel.dausend <marcel.dausend@uni-ulm.de>

---
+++ org.coreasm.engine/test/org/coreasm/engine/test/TestAllCasm.java
@@ -0,0 +1,297 @@
+package org.coreasm.engine.test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.coreasm.util.Tools;
+
+public class TestAllCasm {
+
+	protected static List<File> testFiles = null;
+
+	@BeforeClass
+	public static void onlyOnce() {
+		URL url = TestAllCasm.class.getClassLoader().getResource("./without_test_class");
+
+		try {
+			testFiles = new LinkedList<File>();
+			getTestFiles(testFiles, new File(url.toURI()));
+		}
+		catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private final ByteArrayOutputStream logContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final static PrintStream origOutput = System.out;
+	private final static PrintStream origError = System.err;
+
+	public static List<String> getFilteredOutput(File file, String filter) {
+		List<String> requiredOutputList = new LinkedList<String>();
+		BufferedReader input = null;
+		try {
+			input = new BufferedReader(new FileReader(file));
+			String line = null; //not declared within while loop
+			while ((line = input.readLine()) != null) {
+				if (Pattern.matches(".*" + filter + ".*", line)) {
+					int first = line.indexOf("\"");
+					int last = line.lastIndexOf("\"");
+					if (first >= 0 && last > first)
+						requiredOutputList.add(Tools.convertFromEscapeSequence(line.substring(first + 1, last)));
+				}
+			}
+			input.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return requiredOutputList;
+	}
+
+	protected static void getTestFile(List<File> testFiles, File file, Class<?> clazz) {
+		if (!testFiles.isEmpty())
+			return;
+		if (file != null && file.isDirectory())
+			for (File child : file.listFiles(new FileFilter() {
+
+				@Override
+				public boolean accept(File file) {
+					return (file.isDirectory()
+							|| file.getName().toLowerCase().endsWith(".casm")
+							|| file.getName().toLowerCase().endsWith(".coreasm"));
+				}
+			})) {
+				getTestFile(testFiles, child, clazz);
+			}
+		else if (file != null
+				&& file.getName().toLowerCase().matches(clazz.getSimpleName().toLowerCase() + "(.casm|.coreasm)"))
+			testFiles.add(file);
+	}
+
+	static void getTestFiles(List<File> testFiles, File file) {
+		if (file != null && file.isDirectory())
+			for (File child : file.listFiles(new FileFilter() {
+
+				@Override
+				public boolean accept(File file) {
+					return (file.isDirectory()
+							|| file.getName().toLowerCase().endsWith(".casm")
+							|| file.getName().toLowerCase().endsWith(".coreasm"));
+				}
+			})) {
+				getTestFiles(testFiles, child);
+			}
+		else if (file != null)
+			testFiles.add(file);
+	}
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(logContent));
+		System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(origOutput);
+		System.setErr(origError);
+	}
+
+	@Test
+	public void performTest() {
+		TestReport t = null;
+		boolean succesful = true;
+		//check if their are files for testing for this class
+		if (testFiles.isEmpty()) {
+			t = new TestReport(null, "no test file found!", -1, false);
+			succesful = false;
+		}
+		//perform test for all test files, output result, and modify test result if test has been failed
+		for (File testFile : testFiles) {
+			t = runSpecification(testFile);
+			if (!t.successful())
+				succesful = false;
+			t.print();
+			t = null;
+		}
+		//report overall test result
+		//test failed if at least one test has been failed
+		if (!succesful)
+			Assert.fail("Test failed for class: " + TestAllCasm.class.getSimpleName());
+
+	}
+
+	public TestReport runSpecification(File testFile) {
+
+		List<String> requiredOutputList = getFilteredOutput(testFile, "@require");
+		List<String> refusedOutputList = getFilteredOutput(testFile, "@refuse");
+		List<String> minStepsList = getFilteredOutput(testFile, "@minsteps");
+		List<String> maxStepsList = getFilteredOutput(testFile, "@maxsteps");
+		int minSteps = 1;
+		if (!minStepsList.isEmpty()) {
+			try {
+				minSteps = Integer.parseInt(minStepsList.get(0));
+			}
+			catch (NumberFormatException e) {
+			}
+		}
+		int maxSteps = minSteps;
+		if (!maxStepsList.isEmpty()) {
+			try {
+				maxSteps = Integer.parseInt(maxStepsList.get(0));
+			}
+			catch (NumberFormatException e) {
+			}
+		}
+		TestEngineDriver td = null;
+		String failMessage = "";
+		int steps = 0;
+		try {
+			outContent.reset();
+			errContent.reset();
+			td = TestEngineDriver.newLaunch(testFile.getAbsolutePath());
+			if (TestEngineDriver.TestEngineDriverStatus.stopped.equals(td.getStatus()))
+				return new TestReport(
+						testFile, "engine is stopped!", steps, false);
+
+			td.setOutputStream(new PrintStream(outContent));
+			for (steps = minSteps; steps <= maxSteps; steps++) {
+				td.executeSteps(minSteps);
+				minSteps = 1;
+				//test if no error has been occured and maybe output error message
+				if (!errContent.toString().isEmpty()) {
+					failMessage = "An error occurred in " + testFile.getName() + ":" + errContent;
+					return new TestReport(testFile, failMessage, steps, false);
+				}
+				//check if no refused output is contained
+				for (String refusedOutput : refusedOutputList) {
+					if (outContent.toString().contains(refusedOutput)) {
+						failMessage = "refused output found in test file: " + testFile.getName()
+								+ ", refused output: "
+								+ refusedOutput
+								+ ", actual output: " + outContent.toString();
+						return new TestReport(testFile, failMessage, steps, false);
+					}
+				}
+				for (String requiredOutput : new LinkedList<String>(requiredOutputList)) {
+					if (outContent.toString().contains(requiredOutput))
+						requiredOutputList.remove(requiredOutput);
+				}
+				if (requiredOutputList.isEmpty())
+					break;
+			}
+			//check if no required output is missing
+			if (!requiredOutputList.isEmpty()) {
+				failMessage = "missing required output for test file: " + testFile.getName()
+						+ ", missing output: "
+						+ requiredOutputList.get(0)
+						+ ", actual output: " + outContent.toString();
+				return new TestReport(testFile, failMessage, steps - 1, false);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace(origOutput);
+		}
+		finally {
+			td.stop();
+		}
+		if (td.isRunning()) {
+			failMessage = testFile.getName() + " has a running instance but is stopped!";
+			return new TestReport(testFile, failMessage, steps, false);
+		}
+		else if (steps <= maxSteps /* only if successful */)
+			return new TestReport(testFile, steps);
+		else {
+			failMessage = "No test result for test class " + TestAllCasm.class.getSimpleName();
+			return new TestReport(testFile, failMessage, steps, false);
+		}
+	}
+
+	static class TestReport {
+		private static ArrayList<TestReport> reports = new ArrayList<TestReport>();
+		private File file;
+		private String message;
+		private int steps;
+		private boolean successful;
+
+		public TestReport(File file, int steps) {
+			this(file, "", steps);
+		}
+
+		public TestReport(File file, String message, int steps) {
+			this(file, message, steps, true);
+		}
+
+		public TestReport(File file, String message, int steps, boolean successful) {
+			this.file = file;
+			this.message = message;
+			this.successful = successful;
+			this.steps = steps;
+			if (!TestReport.reports.isEmpty()
+					&& TestReport.getLast().getFile() == this.file)
+				origOutput
+						.println("Last report has been for the same file. Check if your test produces a unique result.");
+			reports.add(this);
+		}
+
+		private File getFile() {
+			return this.file;
+		}
+
+		public void print() {
+			if (this.successful) {
+				String success = "Test of " + this.file.getName() + " successful after " + steps
+						+ (steps == 1 ? " step" : " steps");
+				origOutput.println(this.message.isEmpty() ? success : success + "; " + this.message);
+			}
+			else
+				origError.println("An error occurred after " + steps + " steps in " + this.file.getName() + ": "
+						+ this.message);
+		}
+
+		public void printTestReports() {
+			for (TestReport report : reports) {
+				report.print();
+			}
+		}
+
+		public boolean successful() {
+			return this.successful;
+		}
+
+		public static TestReport getLast() {
+			if (reports.isEmpty())
+				return null;
+			else
+				return reports.get(reports.size() - 1);
+		}
+
+		public String getMessage() {
+			return this.message;
+		}
+	}
+
+}
---
+++ org.coreasm.engine/test/org/coreasm/engine/test/TestEngineDriver.java
@@ -0,0 +1,358 @@
+package org.coreasm.engine.test;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.coreasm.engine.CoreASMEngine.EngineMode;
+import org.coreasm.engine.CoreASMEngineFactory;
+import org.coreasm.engine.CoreASMError;
+import org.coreasm.engine.Engine;
+import org.coreasm.engine.EngineErrorEvent;
+import org.coreasm.engine.EngineErrorObserver;
+import org.coreasm.engine.EngineEvent;
+import org.coreasm.engine.EngineProperties;
+import org.coreasm.engine.EngineStepObserver;
+import org.coreasm.engine.StepFailedEvent;
+import org.coreasm.engine.absstorage.Update;
+import org.coreasm.engine.plugin.PluginServiceInterface;
+import org.coreasm.engine.plugins.io.IOPlugin.IOPluginPSI;
+import org.coreasm.util.CoreASMGlobal;
+import org.coreasm.util.Logger;
+import org.coreasm.util.Tools;
+
+public class TestEngineDriver implements Runnable, EngineStepObserver, EngineErrorObserver {
+
+	protected static List<TestEngineDriver> runningInstances = null;
+
+	protected Engine engine;
+
+	public enum TestEngineDriverStatus {
+		stopped, running, paused
+	};
+
+	private TestEngineDriverStatus status = TestEngineDriverStatus.stopped;
+
+	private boolean updateFailed;
+	private String stepFailedMsg;
+	protected CoreASMError lastError;
+	private int stepsLimit;
+	private boolean stopOnEmptyUpdates;
+	private boolean stopOnStableUpdates;
+	private boolean stopOnEmptyActiveAgents;
+	private boolean stopOnFailedUpdates;
+	private boolean stopOnError;
+	private boolean stopOnStepsLimit;
+	private boolean dumpUpdates;
+	private boolean dumpState;
+	private boolean dumpFinal;
+	private boolean markSteps;
+	private boolean printAgents;
+	private boolean shouldStop;
+	private boolean shouldPause;
+
+	private TestEngineDriver() {
+		if (runningInstances == null)
+			runningInstances = new LinkedList<TestEngineDriver>();
+		runningInstances.add(this);
+		CoreASMGlobal.setRootFolder(Tools.getRootFolder());
+		engine = (Engine) org.coreasm.engine.CoreASMEngineFactory.createEngine();
+		engine.addObserver(this);
+		shouldStop = false;
+		shouldPause = true;
+		status = TestEngineDriverStatus.paused;
+
+		String pluginFolders = Tools.getRootFolder(Engine.class)+"/plugins";
+		if (System.getProperty(EngineProperties.PLUGIN_FOLDERS_PROPERTY) != null)
+			pluginFolders += EngineProperties.PLUGIN_FOLDERS_DELIM
+					+ System.getProperty(EngineProperties.PLUGIN_FOLDERS_PROPERTY);
+		engine.setProperty(EngineProperties.PLUGIN_FOLDERS_PROPERTY, pluginFolders);
+		engine.setClassLoader(CoreASMEngineFactory.class.getClassLoader());
+		engine.initialize();
+		engine.waitWhileBusy();
+	}
+
+	public TestEngineDriverStatus getStatus() {
+		return status;
+	}
+
+	public void setOutputStream(PrintStream outputStream) {
+		PluginServiceInterface pi = engine.getPluginInterface("IOPlugin");
+		if (pi != null)
+			((IOPluginPSI)pi).setOutputStream(outputStream);
+	}
+
+	public void setDefaultConfig()
+	{
+		Logger.verbosityLevel = Logger.ERROR;
+		stopOnEmptyUpdates = false;
+		stopOnStableUpdates = false;
+		stopOnEmptyActiveAgents = true;
+		stopOnFailedUpdates = false;
+		stopOnError = false;
+		stopOnStepsLimit = false; 	// TODO this should probably be false
+		stepsLimit = -1; //means infinite steps
+		dumpUpdates = false;
+		dumpState = false;
+		dumpFinal = false;
+		markSteps = false;
+		printAgents = false;
+	}
+
+	public Engine getEngine() {
+		return engine;
+	}
+
+	public boolean isRunning() {
+		return runningInstances.contains(this);
+	}
+
+	public static TestEngineDriver newLaunch(String abspathname) {
+		TestEngineDriver td = new TestEngineDriver();
+		td.setDefaultConfig();
+		td.dolaunch(abspathname);
+		return td;
+	}
+
+	public void dolaunch(String abspathname) {
+		Thread t = new Thread(this);
+
+		try {
+			t.setName("CoreASM run of " +
+					abspathname.substring(abspathname.lastIndexOf(File.separator)));
+		}
+		catch (Throwable e) {
+			t.setName("CoreASM run of " + abspathname);
+		}
+
+		if (engine.getEngineMode() == EngineMode.emError) {
+			engine.recover();
+			engine.waitWhileBusy();
+		}
+
+		engine.loadSpecification(abspathname);
+		engine.waitWhileBusy();
+
+		t.start();
+	}
+
+	@Override
+	public void run()
+	{
+		int step = 0;
+		Exception exception = null;
+
+		Set<Update> updates, prevupdates = null;
+
+		try {
+
+			if (engine.getEngineMode() != EngineMode.emIdle) {
+				handleError();
+				return;
+			}
+
+			while (engine.getEngineMode() == EngineMode.emIdle) {
+
+				//set current mode
+				if (shouldStop && stepsLimit <= 0) {
+					engine.waitWhileBusy();
+					break;
+				}
+				else if (shouldPause || stepsLimit == 0) {
+					status = TestEngineDriverStatus.paused;
+					Thread.sleep(100);
+				}
+				else
+				{
+					status = TestEngineDriverStatus.running;
+
+					//execute a step
+					engine.waitWhileBusy();
+					engine.step();
+					step++;
+					engine.waitWhileBusy();
+
+					updates = engine.getUpdateSet(0);
+					if (markSteps)
+						System.out.println("#--- end of step " + step);
+					if (dumpUpdates)
+						System.out.println("Updates at step " + step + ": " + updates);
+					if (dumpState)
+						System.out.println("State at step " + step + ":\n" + engine.getState());
+					if (printAgents)
+						System.out.println("Last selected agents: " + engine.getLastSelectedAgents());
+					if (terminated(step, updates, prevupdates))
+						break;
+					prevupdates = updates;
+					stepsLimit--;
+				}
+
+			}
+			if (engine.getEngineMode() != EngineMode.emIdle)
+				handleError();
+		}
+		catch (Exception e) {
+			exception = e;
+			e.printStackTrace();
+		}
+		finally {
+			if (runningInstances != null && runningInstances.contains(this)) {
+				runningInstances.remove(this);
+				this.engine.removeObserver(this);
+
+				if (exception != null)
+					System.err.println("[!] Run is terminated with exception " + exception);
+
+				if (dumpFinal && step > 0) {
+					System.out.println("--------------------FINISHED---------------------");
+					System.out.println("Final engine mode was " + this.engine.getEngineMode());
+					if (lastError != null)
+						System.out.println("Last error was " + lastError);
+					if (stepFailedMsg != null)
+						System.out.println("Step failed reason was " + stepFailedMsg);
+					System.out.println("Final state was:\n" + this.engine.getState());
+
+					// Repeating 
+					if (exception != null)
+						System.err.println("[!] Run is terminated with exception " + exception);
+				}
+
+				this.engine.terminate();
+				this.engine.hardInterrupt();
+
+				status = TestEngineDriverStatus.stopped;
+			}
+		}
+	}
+
+	/**
+	 * starts the engine and resets
+	 */
+	public void start() {
+		shouldPause = false;
+		shouldStop = false;
+		stopOnStepsLimit = false;
+		resume();
+	}
+
+	public void restart() {
+		shouldPause = false;
+		shouldStop = false;
+		stopOnStepsLimit = false;
+		stepsLimit = -1;
+		while (getStatus() == TestEngineDriverStatus.running)
+			try {
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+	public void resume() {
+		if (stepsLimit == 0)
+			stepsLimit = -1;
+		shouldPause = false;
+		while (getStatus() == TestEngineDriverStatus.paused)
+			try {
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+	public void stop() {
+		shouldStop = true;
+		while (getStatus() != TestEngineDriverStatus.stopped)
+			try {
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+	public void pause() {
+		shouldPause = true;
+		while (getStatus() == TestEngineDriverStatus.running)
+			try {
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+	public void executeSteps(int numberOfSteps) {
+		stepsLimit = numberOfSteps;
+		stopOnStepsLimit = true;
+		resume();
+		while (stepsLimit > 0 && getStatus() != TestEngineDriverStatus.stopped)
+			try {
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+
+	private boolean terminated(int step, Set<Update> updates, Set<Update> prevupdates) {
+		if (stopOnEmptyUpdates && updates.isEmpty())
+			return true;
+		if (stopOnStableUpdates && updates.equals(prevupdates))
+			return true;
+		if (stopOnEmptyActiveAgents && engine.getAgentSet().size() < 1)
+			return true;
+		if (stopOnFailedUpdates && updateFailed)
+			return true;
+		if (stopOnError && lastError != null)
+			return true;
+		if (stopOnStepsLimit && stepsLimit <= 0 && shouldStop)
+			return true;
+		return false;
+	}
+
+	@Override
+	public void update(EngineEvent event) {
+
+		// Looking for StepFailed
+		if (event instanceof StepFailedEvent) {
+			StepFailedEvent sEvent = (StepFailedEvent) event;
+			synchronized (this) {
+				updateFailed = true;
+				stepFailedMsg = sEvent.reason;
+			}
+		}
+
+		// Looking for errors
+		else if (event instanceof EngineErrorEvent) {
+			synchronized (this) {
+				lastError = ((EngineErrorEvent) event).getError();
+			}
+		}
+
+	}
+
+	protected void handleError() {
+		String message = "";
+		if (lastError != null)
+			message = lastError.showError();
+		else
+			message = "Enginemode should be " + EngineMode.emIdle + " but is " + engine.getEngineMode();
+
+		showErrorDialog("CoreASM Engine Error", message);
+
+		lastError = null;
+		stepFailedMsg = null;
+		engine.recover();
+		engine.waitWhileBusy();
+	}
+
+	private void showErrorDialog(String title, String message) {
+		System.err.println(title + "\n" + message);
+	}
+}
