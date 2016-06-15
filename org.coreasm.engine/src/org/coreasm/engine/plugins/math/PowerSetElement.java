db0527e50e458b2e74453e693efa88167ec94901
Michael Stegmaier <michael-1.stegmaier@uni-ulm.de>
2016-06-15 17:38:28 +0200

    Added an improved implementation of a powerset.
    Signed-off-by: Michael Stegmaier <michael-1.stegmaier@uni-ulm.de>

--- org.coreasm.engine/src/org/coreasm/engine/plugins/math/PowerSetElement.java
+++ org.coreasm.engine/src/org/coreasm/engine/plugins/math/PowerSetElement.java
@@ -1,112 +1,72 @@
-/*
- * PowerSetElement.java 		$Revision: 11 $
- * 
- * Copyright (c) 2007 Roozbeh Farahbod
- *
- * Last modified on $Date: 2009-01-28 11:03:35 +0100 (Mi, 28 Jan 2009) $ by $Author: rfarahbod $
- *
- * Licensed under the Academic Free License version 3.0
- *   http://www.opensource.org/licenses/afl-3.0.php
- *   http://www.coreasm.org/afl-3.0.php
- *
- */
-
 package org.coreasm.engine.plugins.math;
 
 import java.math.BigInteger;
+import java.util.AbstractList;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+import org.coreasm.engine.CoreASMError;
+import org.coreasm.engine.absstorage.BooleanElement;
 import org.coreasm.engine.absstorage.Element;
 import org.coreasm.engine.absstorage.Enumerable;
-import org.coreasm.engine.plugins.set.SetElement;
+import org.coreasm.engine.plugins.collection.AbstractSetElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Provide the powerset of an enumerable.
  *   
- * @author Roozbeh Farahbod
- * @version Last modified on $Date: 2009-01-28 11:03:35 +0100 (Mi, 28 Jan 2009) $
+ * @author Michael Stegmaier
  *
  */
 
 public class PowerSetElement extends Element implements Enumerable, Collection<Element> {
-
-	public static final int MAX_SIZE = 31;
-	
 	protected static final Logger logger = LoggerFactory.getLogger(PowerSetElement.class);
 
 	private final ArrayList<Element> elements;
-	private Set<Element> elementsSet = null;
 	private String denotationalValue = null;
+	
+	private PowerSetIndexedView indexedView = null;
 	
 	public PowerSetElement(Enumerable baseSet) {
 		Collection<? extends Element> base = baseSet.enumerate();
-		if (base.size() > MAX_SIZE)
-			logger.warn( 
-					"MathPlugin: Powerset function over a collection of more than " + MAX_SIZE + " elements.");
+		if (base.size() >= Integer.SIZE - 1)
+			logger.warn("MathPlugin: Powerset function over a collection of more than " + (Integer.SIZE - 2) + " elements.");
 		elements = new ArrayList<Element>(base);
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.coreasm.engine.absstorage.Enumerable#contains(org.coreasm.engine.absstorage.Element)
-	 */
+	@Override
 	public boolean contains(Element e) {
-		// Make sure you change both contians methods
-		return containsElement(e);
+		return contains((Object)e);
 	}
 
+	@Override
 	public boolean contains(Object o) {
-		// Make sure you change both contians methods
-		return containsElement(o);
-	}
-
-	/*
-	 * Single implementation for both 
-	 * contains(Element) and contains(Object)
-	 */
-	private boolean containsElement(Object o) {
-		if (o instanceof Enumerable) {
-			for (Element element: ((Enumerable)o).enumerate()) 
-				if (!elements.contains(element))
-					return false;
-			return true;
-		} else
+		if (!(o instanceof Enumerable))
 			return false;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.coreasm.engine.absstorage.Enumerable#enumerate()
-	 */
-	public Collection<Element> enumerate() {
-		return this;
-	}
-
-	/**
-	 * @return the base set of this powerset element
-	 */
-	public Set<Element> getBaseSet() {
-		if (elementsSet == null)
-			elementsSet = new HashSet<Element>(elements);
-		return elementsSet;
+		for (Element element: ((Enumerable)o).enumerate()) {
+			if (!elements.contains(element))
+				return false;
+		}
+		return true;
 	}
 	
 	@Override
-	public boolean equals(Object anElement) {
-		if (anElement instanceof PowerSetElement) {
-			return this.getBaseSet().equals(((PowerSetElement)anElement).getBaseSet());
-		} else
-			if (anElement instanceof Enumerable) {
-				return this.enumerate().equals(((Enumerable)anElement).enumerate());
-			} else
-				return super.equals(anElement);
+	public Collection<Element> enumerate() {
+		if (supportsIndexedView())
+			return getIndexedView();
+		return this;
 	}
 
 	@Override
@@ -114,228 +74,374 @@ public class PowerSetElement extends Element implements Enumerable, Collection<E
 		return elements.hashCode();
 	}
 
-	public boolean add(Element o) {
-		throw new UnsupportedOperationException("Cannot add an element to a powerset value.");
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (getClass() != obj.getClass())
+			return false;
+		PowerSetElement other = (PowerSetElement) obj;
+		return elements.equals(other.elements);
 	}
 
-	public boolean addAll(Collection<? extends Element> c) {
-		throw new UnsupportedOperationException("Cannot add an element to a powerset value.");
-	}
-
-	public void clear() {
-		throw new UnsupportedOperationException("Cannot clear a powerset.");
-	}
-
+	@Override
 	public boolean containsAll(Collection<?> c) {
-		for (Object obj: c)
+		for (Object obj: c) {
 			if (!contains(obj))
 				return false;
+		}
 		return true;
 	}
-
+	
+	@Override
 	public boolean isEmpty() {
-		return elements.isEmpty();
+		return false;
 	}
-
-	public Iterator<Element> iterator() {
-		return new PowerSetIterator(elements);
-	}
-
-	public boolean remove(Object o) {
-		throw new UnsupportedOperationException("Cannot remove an element from powerset value.");
-	}
-
-	public boolean removeAll(Collection<?> c) {
-		throw new UnsupportedOperationException("Cannot remove an element from powerset value.");
-	}
-
-	public boolean retainAll(Collection<?> c) {
-		throw new UnsupportedOperationException("PowerSet.retainAll(...) is not supported.");
-	}
-
+	
+	@Override
 	public int size() {
-		if (elements.size() > MAX_SIZE) {
-			logger.warn(
-					"MathPlugin: Cannot return the size of the Powerset function over a collection of more than " + MAX_SIZE + " elements.");
+		if (elements.size() >= Integer.SIZE - 1)
 			return Integer.MAX_VALUE;
-		} else 
-			return (int)Math.pow(2, elements.size());
+		return 1 << elements.size();
 	}
 
+	@Override
 	public Object[] toArray() {
-		Object[] result = new Object[this.size()];
+		if (elements.size() >= Integer.SIZE - 1)
+			throw new CoreASMError("The base set of this powerset contains " + elements.size() + " elements. But this operation is only supported for up to " + (Integer.SIZE - 2) + " elements.");
+		Object[] result = new Object[size()];
 		int i = 0;
 		for (Element e : this) {
 			result[i] = e;
 			i++;
 		}
-//		throw new UnsupportedOperationException("PowerSet.toArray(...) is not supported.");
 		return result;
 	}
 
 	@SuppressWarnings("unchecked")
+	@Override
 	public <T> T[] toArray(T[] a) {
-		int size = this.size();
-        if (a.length < size)
-            a = (T[])java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+		if (elements.size() >= Integer.SIZE - 2)
+			throw new CoreASMError("The base set of this powerset contains " + elements.size() + " elements. But this operation is only supported for up to " + (Integer.SIZE - 2) + " elements.");
+		final int size = size();
+		if (a.length < size)
+			return (T[])toArray();
 		int i = 0;
 		for (Element e : this) {
 			a[i] = (T)e;
 			i++;
 		}
-        if (a.length > size)
-            a[size] = null;
+		if (i < a.length)
+			return Arrays.copyOf(a, i);
         return a;
 	}
 
-	/**
-	 * Expands the powerset to a string
-	 * representation.
-	 */
+	@Override
 	public String toString() {
-		StringBuffer result = new StringBuffer();
-		for (Element e: this)
-			result.append(", " + e.toString());
-		if (result.length() > 0)
-			return "{" + result.toString().substring(2) + "}";
-		else
-			return "{}";
+		String string = "";
+		for (Element member : this)
+			string += (string.isEmpty() ? " " : ", ") + member;
+		return "{" + string + " }";
 	}
 	
+	@Override
 	public String denotation() {
 		if (denotationalValue == null) {
-			denotationalValue = "P({";
-			for (Element e: elements)
-				denotationalValue = denotationalValue + e.denotation() + ", ";
-			if (elements.size() > 0)
-				denotationalValue = denotationalValue.substring(0, denotationalValue.length() -2);
-			denotationalValue = denotationalValue + "})";
+			denotationalValue = "";
+			for (Element e : elements)
+				denotationalValue += (denotationalValue.isEmpty() ? " " : ", ") + e.denotation();
+			denotationalValue = "P({" + denotationalValue + " })";
 		}
 		return denotationalValue;
 	}
-
-	/**
-	 * The iterator for powerset elements. It 
-	 * creates the next element in every call to the
-	 * <code>next()</code> function.
-	 *
-	 */
-	private static class PowerSetIterator implements Iterator<Element>  {
-
-		private final ArrayList<Element> baseList;
-		private final int baseSize;
 	
-		// having two indices for large base sets and small base sets
-		// is just to increase the performance on small base sets
+	@Override
+	public Iterator<Element> iterator() {
+		return new PowerSetIterator();
+	}
+
+	private class PowerSetIterator implements Iterator<Element>  {
 		private final BigInteger powersetSize;
-		private final int smallPowersetSize; 
+		private final long smallPowersetSize; 
 		private final boolean overSizeBaseSet;
 		private BigInteger bigIndex;
 		private long smallIndex;
 		
-		public PowerSetIterator(ArrayList<Element> baseList) {
-			this.baseList = baseList;
-			this.baseSize = baseList.size();
-			overSizeBaseSet = (baseSize > 63);
+		public PowerSetIterator() {
+			final int baseSize = elements.size();
+			overSizeBaseSet = baseSize >= Long.SIZE - 1;
 			if (overSizeBaseSet) {
-				powersetSize = (new BigInteger("2")).pow(baseSize);
-				smallPowersetSize = Integer.MAX_VALUE;
+				powersetSize = BigInteger.ONE.shiftLeft(baseSize);
+				smallPowersetSize = -1L;
 			} else {
-				smallPowersetSize = (int)Math.pow(2, baseSize);
+				smallPowersetSize = 1L << baseSize;
 				powersetSize = null;
 			}
 			bigIndex = BigInteger.ZERO;
 			smallIndex = 0;
-			
 		}
 
+		@Override
 		public boolean hasNext() {
-			if (overSizeBaseSet)
-				return bigIndex.compareTo(powersetSize) < 0;
-			else
-				return smallIndex < smallPowersetSize;  
+			return overSizeBaseSet && bigIndex.compareTo(powersetSize) < 0 || smallIndex < smallPowersetSize;  
 		}
 
+		@Override
 		public Element next() {
-			if (hasNext()) {
-				Collection<Element> resultSet = new ArrayList<Element>();
-
-				if (overSizeBaseSet) {
-					// if the base set is too large then 
-					// we need to use BigIntegers to compute the next element of 
-					// the powerset
-					BigInteger indices = bigIndex;
-					int ci = 0;
-					while (indices.compareTo(BigInteger.ZERO) > 0) {
-						if (indices.and(BigInteger.ONE).compareTo(BigInteger.ZERO) > 0)
-							resultSet.add(baseList.get(ci));
-						indices.shiftRight(1);
-						ci++;
-					}
-					bigIndex.add(BigInteger.ONE);
-				} else {
-					// if the base set has less than or equal to 32 elements
-					// then we can use basic integers to compute the next lement
-					// of the powerset function
-					long indices = smallIndex;
-					int ci = 0;
-					while (indices > 0) {
-						if ((indices & 1) > 0) 
-							resultSet.add(baseList.get(ci));
-						indices = indices >> 1;
-						ci++;
-					}
-					smallIndex++;
-				}
-				
-				Set<Element> result = new HashSet<Element>();
-				for (Element e: resultSet)
-					result.add(e);
-				return new SetElement(result);
-
-			} else
-				throw new NoSuchElementException("No more elements in the powerset.");
+			if (!hasNext())
+				throw new NoSuchElementException();
+			if (overSizeBaseSet) {
+				PowerSetMember value = new PowerSetMember(bigIndex);
+				bigIndex.add(BigInteger.ONE);
+				return value;
+			}
+			return new PowerSetMember(smallIndex++);
 		}
-
-		public void remove() {
-			throw new UnsupportedOperationException("Cannot remove elements from powerset iterators.");
-		}
-		
 	}
 	
-	@SuppressWarnings("unused")
-	private static class PowerSetMember extends SetElement {
-
-		private final Collection<Element> value;
+	private class PowerSetMember extends AbstractSetElement {
+		private final BitSet keys = new BitSet();
 		
-		public PowerSetMember(Collection<Element> value) {
-			this.value = value;
+		public PowerSetMember(int index) {
+			for (int i = elements.size() - 1; i >= 0; i--) {
+				if ((index & (1 << i)) != 0)
+					keys.set(i);
+			}
 		}
 		
+		public PowerSetMember(long index) {
+			for (int i = elements.size() - 1; i >= 0; i--) {
+				if ((index & (1L << i)) != 0)
+					keys.set(i);
+			}
+		}
+		
+		public PowerSetMember(BigInteger index) {
+			for (int i = elements.size() - 1; i >= 0; i--) {
+				if ((index.and(BigInteger.ONE.shiftLeft(i))).compareTo(BigInteger.ZERO) != 0)
+					keys.set(i);
+			}
+		}
+
+		@Override
+		public Collection<? extends Element> enumerate() {
+			return keySet();
+		}
+
+		@Override
 		public boolean contains(Element e) {
-			return value.contains(e);
+			return containsKey(e);
 		}
 
-		public Collection<Element> enumerate() {
-			return Collections.unmodifiableCollection(value);
+		@Override
+		public int size() {
+			return keys.cardinality();
 		}
-	
+
+		@Override
+		public boolean supportsIndexedView() {
+			return false;
+		}
+
+		@Override
+		public List<Element> getIndexedView() throws UnsupportedOperationException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public AbstractSetElement getNewInstance(Collection<? extends Element> set) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Set<? extends Element> getSet() {
+			return keySet();
+		}
+
+		@Override
+		public Map<Element, Element> getMap() {
+			return new AbstractMap<Element, Element>() {
+
+				@Override
+				public Set<Entry<Element, Element>> entrySet() {
+					return new AbstractSet<Entry<Element,Element>>() {
+
+						@Override
+						public Iterator<Entry<Element, Element>> iterator() {
+							return new Iterator<Entry<Element,Element>>() {
+								private Iterator<Element> keyIterator = keySet().iterator();
+
+								@Override
+								public boolean hasNext() {
+									return keyIterator.hasNext();
+								}
+
+								@Override
+								public Entry<Element, Element> next() {
+									final Element key = keyIterator.next();
+									return new Entry<Element, Element>() {
+
+										@Override
+										public Element getKey() {
+											return key;
+										}
+
+										@Override
+										public Element getValue() {
+											return BooleanElement.TRUE;
+										}
+
+										@Override
+										public Element setValue(Element value) {
+											throw new UnsupportedOperationException();
+										}
+									};
+								}
+							};
+						}
+
+						@Override
+						public int size() {
+							return PowerSetMember.this.size();
+						}
+					};
+				}
+			};
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return keys.isEmpty();
+		}
+
+		@Override
+		public boolean containsKey(Element key) {
+			return keys.get(elements.indexOf(key));
+		}
+
+		@Override
+		public boolean containsValue(Element value) {
+			return BooleanElement.TRUE.equals(value) && !isEmpty() || BooleanElement.FALSE.equals(value);
+		}
+
+		@Override
+		public Element get(Element key) {
+			return BooleanElement.valueOf(containsKey(key));
+		}
+
+		@Override
+		public Set<? extends Element> keySet() {
+			return new AbstractSet<Element>() {
+
+				@Override
+				public Iterator<Element> iterator() {
+					return new Iterator<Element>() {
+						private int cursor = keys.nextSetBit(0);
+
+						@Override
+						public boolean hasNext() {
+							return cursor >= 0 && cursor < Integer.MAX_VALUE;
+						}
+
+						@Override
+						public Element next() {
+							if (!hasNext())
+								throw new NoSuchElementException();
+							int i = cursor;
+							cursor = keys.nextSetBit(cursor + 1);
+							return elements.get(i);
+						}
+					};
+				}
+				
+				@Override
+				public boolean contains(Object o) {
+					if (!(o instanceof Element))
+						return false;
+					return containsKey((Element)o);
+				}
+
+				@Override
+				public int size() {
+					return PowerSetMember.this.size();
+				}
+			};
+		}
+
+		@Override
+		public Collection<? extends Element> values() {
+			return Collections.unmodifiableSet(new HashSet<Element>(Arrays.asList((isEmpty() ? new Element[] { BooleanElement.FALSE } : new Element[] { BooleanElement.FALSE, BooleanElement.TRUE }))));
+		}
+
+		@Override
 		public String toString() {
-			StringBuffer result = new StringBuffer();
-			for (Element e: value)
-				result.append(", " + e.toString());
-			if (result.length() > 0)
-				return "{" + result.toString().substring(2) + "}";
-			else
-				return "{}";
+			String string = "";
+			for (Element key : keySet())
+				string += (string.isEmpty() ? " " : ", ") + key;
+			return "{" + string + " }";
+		}
+
+		@Override
+		public int hashCode() {
+			return keys.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (getClass() != obj.getClass())
+				return false;
+			PowerSetMember other = (PowerSetMember) obj;
+			return keys.equals(other.keys);
+		}
+	}
+	
+	private class PowerSetIndexedView extends AbstractList<Element> implements List<Element> {
+		private final int size = PowerSetElement.this.size();
+
+		@Override
+		public int size() {
+			return size;
+		}
+		
+		@Override
+		public Element get(int index) {
+			return new PowerSetMember(index);
 		}
 	}
 
 	public List<Element> getIndexedView() throws UnsupportedOperationException {
-		throw new UnsupportedOperationException("Powerset is not an indexed collection.");
+		if (indexedView != null)
+			return indexedView;
+		return indexedView = new PowerSetIndexedView();
 	}
 
 	public boolean supportsIndexedView() {
-		return false;
+		return elements.size() < Integer.SIZE - 1;
+	}
+	
+	public boolean add(Element o) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean addAll(Collection<? extends Element> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
 	}
 }
